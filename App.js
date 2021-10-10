aed3f0f619b4f16ccc59032eef900e8a6fb9d6bf
muhammad Waqar <muhammadwaqar552@gmail.com>
2021-10-10 18:58:05 +0400

    flex design in react native

--- App.js
+++ App.js
@@ -1,11 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.heading}>
+        <Text style={styles.text}>My First App</Text>
+      </View>
+      <View style={styles.head}>
+        <Text style={styles.text}>My First App Head Section</Text>
+      </View>
+      <View style={styles.body}>
+        <View style={[styles.bodyx, styles.body1]}>
+          <Text style={styles.text}>Body 1</Text>
+        </View>
+        <View style={[styles.bodyx, styles.body2]}>
+          <Text style={styles.text}>Body 2</Text>
+        </View>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +27,43 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+  },
+  heading: {
+    flex: 1,
+    backgroundColor: "lightgrey",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  head: {
+    flex: 1,
+    backgroundColor: "red",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  body: {
+    flex: 1,
+    flexDirection: "row",
+    backgroundColor: "pink",
+  },
+  bodyx: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  body1: {
+    backgroundColor: "yellow",
+  },
+  body2: {
+    backgroundColor: "green",
+  },
+  headingtext: {
+    fontWeight: "bold",
+    fontSize: 20,
+    padding: 10,
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: "bold",
   },
 });
